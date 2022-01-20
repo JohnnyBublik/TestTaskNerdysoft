3b78162709295c4eab4f4d8fbfb58a6408cd2c06
JohnnyBublik <theeijohnka2003@gmail.com>
2022-01-20 15:37:45 +0200

    updated similar words

--- TestTaskNerdysoft/Controllers/HomeController.cs
+++ TestTaskNerdysoft/Controllers/HomeController.cs
@@ -60,7 +60,104 @@ namespace TestTaskNerdysoft.Controllers
         [HttpGet]
         public ActionResult Show(int id)
         {
-            ViewBag.Announcement = announcementContext.Announcements.Find(id);
+            Announcement announcement1 = announcementContext.Announcements.Find(id);
+            IEnumerable<Announcement> announcements = announcementContext.Announcements;
+            List<Announcement> announcements2 = new List<Announcement>();
+            ViewBag.Announcement = announcements;
+
+            string wordname1 = "";
+            announcement1.Name += " ";
+            announcement1.Description += " ";
+            for (int i = 0; i < announcement1.Name.Length; i++)
+            {
+                if (announcement1.Name[i] != ' ')
+                {
+                    wordname1 += announcement1.Name[i];
+                }
+                else // Есть слово из имени доминирующего объекта
+                {
+                    foreach (var announcementj in announcements) // Проход по вторичным объектам
+                    {
+                        if (announcementj != announcement1)
+                        {
+                            if (announcementj.Name.Contains(wordname1))
+                            {
+                                string worddesc1 = "";
+                                foreach (char symbolofdesc1 in announcement1.Description)
+                                {
+                                    if (symbolofdesc1 != ' ')
+                                    {
+                                        worddesc1 += symbolofdesc1;
+                                    }
+                                    else //Есть слово из описания доминирующего объекта
+                                    {
+                                        if (announcementj.Description.Contains(worddesc1))
+                                        {
+                                            announcements2.Add(announcementj);
+                                            break;
+                                        }
+                                        worddesc1 = "";
+                                    }
+                                }
+                            }
+                            /*if(announcementj!=announcement1)
+                            { 
+                            string wordname2 = "";
+                            foreach (char symbolofname2 in announcementj.Name)
+                            {
+                                    if (symbolofname2 != ' ')
+                                    {
+                                        wordname2 += symbolofname2;
+                                    }
+                                    else //Есть слово из имени вторичного объекта
+                                    {
+
+                                        if (wordname1 == wordname2) // Проверка двух слов из имени
+                                        {
+                                            string worddesc1 = "";
+                                            foreach (char symbolofdesc1 in announcement1.Description)
+                                            {
+                                                if (symbolofdesc1 != ' ')
+                                                {
+                                                    worddesc1 += symbolofdesc1;
+                                                }
+                                                else //Есть слово из описания доминирующего объекта
+                                                {
+                                                    string worddesc2 = "";
+                                                    foreach (char symbolofdesc2 in announcementj.Description)
+                                                    {
+                                                        if (symbolofdesc2 != ' ')
+                                                        {
+                                                            worddesc2 += symbolofdesc2;
+                                                        }
+                                                        else
+                                                        {
+                                                            if (worddesc1 == worddesc2)
+                                                            {
+                                                                announcements2.Add(announcementj);
+                                                                break;
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }*/
+                        }
+                    }
+                    wordname1 = "";
+                }
+
+                if (announcements2.Count == 3)
+                    break;
+            }
+            announcement1.Name.Trim();
+            announcement1.Description.Trim();
+            ViewBag.Announcement = announcement1;
+            IEnumerable<Announcement> announcementssimilar = announcements2;
+            ViewBag.AnnouncementSimilar = announcementssimilar;
             return View();
         }
         [HttpPost]
